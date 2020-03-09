bdf821604a1512dea48360c36ba96604ce3db941
Sergio Robles <sroblesc@gmail.com>
2020-03-09 16:39:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let element = document.createElement('div')
+
+let ul = document.createElement('ul')
+ 
+let main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion';
